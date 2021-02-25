3911f086d743eca3d5baa1c6daacb3dbbdf31479
vedransehovic <vedran.sehovic@gmail.com>
2021-02-24 21:25:55 -0500

    FAQ Section Done

---
+++ src/components/FaqSection.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ut
+          laboriosam aliquid laborum quos eius facere consequatur molestias
+          voluptates vero accusantium magni cupiditate, mollitia iste, placeat
+          sit laudantium illum a.
+        </p>
+      </div>
+      <div className="question">
+        <h4>What products do you offer</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ut
+          laboriosam aliquid laborum quos eius facere consequatur molestias
+          voluptates vero accusantium magni cupiditate, mollitia iste, placeat
+          sit laudantium illum a.
+        </p>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ut
+          laboriosam aliquid laborum quos eius facere consequatur molestias
+          voluptates vero accusantium magni cupiditate, mollitia iste, placeat
+          sit laudantium illum a.
+        </p>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ut
+          laboriosam aliquid laborum quos eius facere consequatur molestias
+          voluptates vero accusantium magni cupiditate, mollitia iste, placeat
+          sit laudantium illum a.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //import sections
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
