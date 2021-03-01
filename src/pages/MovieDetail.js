54d3f8afe5ad51700f35d0aa72d0f52705990650
vedransehovic <vedran.sehovic@gmail.com>
2021-02-28 20:41:23 -0500

    Movie Detail State

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import { MovieState } from "../moviestate";
 
 const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  //UseEffect
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie);
+  }, [movies, url]);
+
   return (
     <div>
       <h2>Movie Detail</h2>
