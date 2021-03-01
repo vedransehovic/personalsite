dc051140bdbf7dfb92c6976cfac59e15661997bf
vedransehovic <vedran.sehovic@gmail.com>
2021-02-28 20:17:28 -0500

    Movie Detail Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 //Router
 import { Switch, Route } from "react-router-dom";
 
@@ -18,11 +19,14 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
         </Route>
         <Route path="/contact">
           <ContactUs />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
       </Switch>
     </div>
---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
+const MovieDetail = () => {
+  return (
+    <div>
+      <h2>Movie Detail</h2>
+    </div>
+  );
+};
+
+export default MovieDetail;
