857be95522660a7d13ea035c846fa6af47a9f705
vedransehovic <vedran.sehovic@gmail.com>
2021-03-01 20:42:39 -0500

    Our Work - Link Update

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -12,21 +12,21 @@ const OurWork = () => {
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-athlete">
           <img src={athlete} alt="athlete" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/good-times">
           <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
@@ -35,6 +35,7 @@ const OurWork = () => {
 };
 
 const Work = styled.div`
+  color: white;
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
