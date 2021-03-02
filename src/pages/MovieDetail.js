039f25a258c88df691e953e9634c84c18502f401
vedransehovic <vedran.sehovic@gmail.com>
2021-03-01 20:38:30 -0500

    Movie Detail

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../moviestate";
 
@@ -12,13 +12,97 @@ const MovieDetail = () => {
   //UseEffect
   useEffect(() => {
     const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
-    setMovie(currentMovie);
+    setMovie(currentMovie[0]);
   }, [movies, url]);
 
   return (
-    <div>
-      <h2>Movie Detail</h2>
-    </div>
+    <>
+      {movie && (
+        <Details>
+          <HeadLine>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </HeadLine>
+          <Awards>
+            {movie.awards.map((award) => (
+              <Award
+                title={award.title}
+                description={award.description}
+                key={award.title}
+              />
+            ))}
+          </Awards>
+          <ImageDisplay>
+            <img src={movie.secondaryImg} alt="movie" />
+          </ImageDisplay>
+        </Details>
+      )}
+    </>
+  );
+};
+
+const Details = styled.div`
+  color: white;
+`;
+
+const HeadLine = styled.div`
+  min-height: 90vh;
+  padding-top: 20vh;
+  position: relative;
+  h2 {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Awards = styled.div`
+  min-height: 80vh;
+  display: flex;
+  margin: 5rem 10rem;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const AwardStyle = styled.div`
+  padding: 5rem;
+  h3 {
+    font-size: 2rem;
+  }
+  .line {
+    width: 100%;
+    background: #23d997;
+    height: 0.5rem;
+    margin: 1rem 0rem;
+  }
+  p {
+    padding: 2rem 0rem;
+  }
+`;
+
+const ImageDisplay = styled.div`
+  min-height: 50vh;
+  img {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+`;
+
+//Award Component
+const Award = ({ title, description }) => {
+  return (
+    <AwardStyle>
+      <h3>{title}</h3>
+      <div className="line"></div>
+      <p>{description}</p>
+    </AwardStyle>
   );
 };
 
